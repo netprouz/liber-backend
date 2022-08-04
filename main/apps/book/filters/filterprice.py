83b3306b48ed583a01c0a81c16c42fc94bc36a94
akhad~ <ahadjon.abdullaev1997@gmail.com>
2022-08-04 23:03:32 +0500

    changes

--- main/apps/book/filters/filterprice.py
+++ main/apps/book/filters/filterprice.py
@@ -5,37 +5,35 @@ from django_filters import NumberFilter
 from django_filters import filters
 
 
-class BookFilter(FilterSet):
-    min_price = NumberFilter(field_name='price', lookup_expr='gte', label='Min Price')
-    max_price = NumberFilter(field_name='price', lookup_expr='lte', label="Max Price")
-    category = filters.CharFilter(field_name='book__category__title', lookup_expr='iexact', label='Category') 
-    book_type = filters.ModelChoiceFilter(field_name='book_type',label="Book type", choices=TYPEChoices, queryset=BookType.objects.all())
-
-    class Meta:
-        model = BookType
-        fields = (
-            'min_price',
-            'max_price',
-            'category', 
-            'book_type'
-            )
-
 # class BookFilter(FilterSet):
-#     category = filters.CharFilter(field_name='category__id', lookup_expr='iexact', label='Category')
-#     published_date = filters.DateFilter(field_name='published_date', input_formats=[
-#         "%Y", "%Y"], lookup_expr='iexact', label='Published date')
-#     min_price = NumberFilter(field_name='types__book_type__price', lookup_expr='gte')
-#     max_price = NumberFilter(field_name='types__book_type__price', lookup_expr='lte')
-#     # book_type = filters.ChoiceFilter(field_name='book_type', choices=TYPEChoices)
+#     min_price = NumberFilter(field_name='price', lookup_expr='gte', label='Min Price')
+#     max_price = NumberFilter(field_name='price', lookup_expr='lte', label="Max Price")
+#     category = filters.CharFilter(field_name='book__category__title', lookup_expr='iexact', label='Category') 
+#     book_type = filters.ModelChoiceFilter(field_name='book_type',label="Book type", choices=TYPEChoices, queryset=BookType.objects.all())
 
 #     class Meta:
-#         model = Book
+#         model = BookType
 #         fields = (
 #             'min_price',
 #             'max_price',
-#             'category',
-#             # 'book_type',
-#             'published_date'
+#             'category', 
+#             'book_type'
 #             )
+
+class BookFilter(FilterSet):
+    category = filters.CharFilter(field_name='category__title', lookup_expr='iexact', label='Category')
+    min_price = NumberFilter(field_name='types__price', lookup_expr='gte')
+    max_price = NumberFilter(field_name='types__price', lookup_expr='lte')
+    book_type = filters.CharFilter(field_name='types__book_type', label="Book Type")
+
+    class Meta:
+        model = Book
+        fields = (
+            'min_price',
+            'max_price',
+            'category',
+            # 'book_type',
+            # 'published_date'
+            )
 
 
