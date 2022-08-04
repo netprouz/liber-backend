f369fcb1ae7ba7a26d9c6a70092bdca0d487e5de
akhad~ <ahadjon.abdullaev1997@gmail.com>
2022-08-04 17:53:44 +0500

    last change

--- main/apps/book/filters/filterprice.py
+++ main/apps/book/filters/filterprice.py
@@ -22,8 +22,8 @@ class BookFilter(FilterSet):
     category = filters.CharFilter(field_name='category__id', lookup_expr='iexact', label='Category')
     # published_date = django_filters.DateFilter(field_name='published_date', input_formats=[
     #     "%Y", "%Y"], lookup_expr='gte', label='Published date')
-    min_price = NumberFilter(field_name='types__price', lookup_expr='gte', )
-    max_price = NumberFilter(field_name='price', lookup_expr='lte', )
+    min_price = NumberFilter(field_name='types__price', lookup_expr='gte')
+    max_price = NumberFilter(field_name='types__price', lookup_expr='lte')
     # book_type = filters.ChoiceFilter(field_name='book_type', choices=TYPEChoices)
 
     class Meta:
