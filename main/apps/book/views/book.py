20210f8912daca9a5c6c6d47f29e54f665bb8242
Kahyitov-Dev <webdewoloper@gmail.com.com>
2022-09-24 18:09:57 +0500

    Bismillah

--- main/apps/book/views/book.py
+++ main/apps/book/views/book.py
@@ -66,67 +66,67 @@ class RelatedBooksListAPIView(generics.ListAPIView):
 related_book_api_view = RelatedBooksListAPIView.as_view()
 
 class BookDetailAPIView(generics.RetrieveAPIView):
-    lookup_field = 'guid'
-    queryset = Book.objects.all()
-    serializer_class = BookDetailSerializer
-    # def get(self, request, guid):
-    #         review_count = Review.objects.filter(book__guid=self.kwargs['guid'])
-    #         rate_avg = Review.objects.filter(book__guid=guid).aggregate(Avg('point'))
+    # lookup_field = 'guid'
+    # queryset = Book.objects.all()
+    # serializer_class = BookDetailSerializer
+    def get(self, request, guid):
+            review_count = Review.objects.filter(book__guid=self.kwargs['guid'])
+            rate_avg = Review.objects.filter(book__guid=guid).aggregate(Avg('point'))
 
-    #         point_one = Review.objects.filter(book__guid=self.kwargs['guid'], point='1').count()
-    #         point_two = Review.objects.filter(book__guid=self.kwargs['guid'], point='2').count()
-    #         point_three = Review.objects.filter(book__guid=self.kwargs['guid'], point='3').count()
-    #         point_four = Review.objects.filter(book__guid=self.kwargs['guid'], point='4').count()
-    #         point_five = Review.objects.filter(book__guid=self.kwargs['guid'], point='5').count()
+            point_one = Review.objects.filter(book__guid=self.kwargs['guid'], point='1').count()
+            point_two = Review.objects.filter(book__guid=self.kwargs['guid'], point='2').count()
+            point_three = Review.objects.filter(book__guid=self.kwargs['guid'], point='3').count()
+            point_four = Review.objects.filter(book__guid=self.kwargs['guid'], point='4').count()
+            point_five = Review.objects.filter(book__guid=self.kwargs['guid'], point='5').count()
 
-    #         total = point_one + point_two + point_three + point_four + point_five
+            total = point_one + point_two + point_three + point_four + point_five
 
-    #         point_one_percent = (point_one/total)*100
-    #         point_two_percent = (point_two/total)*100
-    #         point_three_percent = (point_three/total)*100
-    #         point_four_percent = (point_four/total)*100
-    #         point_five_percent = (point_five/total)*100
+            point_one_percent = (point_one/total)*100
+            point_two_percent = (point_two/total)*100
+            point_three_percent = (point_three/total)*100
+            point_four_percent = (point_four/total)*100
+            point_five_percent = (point_five/total)*100
 
-    #         for key in rate_avg.keys():
-    #             rate_avg[key] = round(rate_avg[key], 1)
+            for key in rate_avg.keys():
+                rate_avg[key] = round(rate_avg[key], 1)
 
-    #         data = {
-    #             'review_count': review_count.count(),
-    #             'rate': rate_avg[key],
+            data = {
+                'review_count': review_count.count(),
+                'rate': rate_avg[key],
 
-    #             # rate number
-    #             "point_one": point_one,
-    #             "point_two": point_two,
-    #             "point_three": point_three,
-    #             "point_four": point_four,
-    #             "point_five": point_five,
+                # rate number
+                "point_one": point_one,
+                "point_two": point_two,
+                "point_three": point_three,
+                "point_four": point_four,
+                "point_five": point_five,
 
-    #             # percent
-    #             'point_one_percent': round(point_one_percent),
-    #             'point_two_percent': round(point_two_percent),
-    #             'point_three_percent': round(point_three_percent),
-    #             'point_four_percent': round(point_four_percent),
-    #             'point_five_percent': round(point_five_percent),
-    #         }
+                # percent
+                'point_one_percent': round(point_one_percent),
+                'point_two_percent': round(point_two_percent),
+                'point_three_percent': round(point_three_percent),
+                'point_four_percent': round(point_four_percent),
+                'point_five_percent': round(point_five_percent),
+            }
 
-    #         book = Book.objects.get(guid=guid)
-    #         serializer = BookDetailSerializer(book)
+            book = Book.objects.get(guid=guid)
+            serializer = BookDetailSerializer(book)
 
-            # return Response({'data':data, 'book_detail':serializer.data})
+            return Response({'data':data, 'book_detail':serializer.data})
 
-    # def get_object(self):
-    #     queryset = self.filter_queryset(self.get_queryset())
-    #     lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-    #     assert lookup_url_kwarg in self.kwargs, (
-    #         self.__class__.__name__,
-    #         lookup_url_kwarg,
-    #     )
-    #     filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-    #     obj = get_object_or_404(queryset, **filter_kwargs)
-    #     # self.check_object_permissions(self.request, obj)
-    #     # count book views
-    #     # count_book_view(book=obj, user=self.request.user)
-    #     return obj
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        assert lookup_url_kwarg in self.kwargs, (
+            self.__class__.__name__,
+            lookup_url_kwarg,
+        )
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = get_object_or_404(queryset, **filter_kwargs)
+        # self.check_object_permissions(self.request, obj)
+        # count book views
+        # count_book_view(book=obj, user=self.request.user)
+        return obj
 
 book_detail_api_view = BookDetailAPIView.as_view()
 
