00b1f33e20c9e3d09cd7946e385791576352cf06
akhad97 <ahadjon.abdullaev1997@gmail.com>
2022-10-03 22:27:42 +0500

    changes

--- main/apps/account/serializers/online_book.py
+++ main/apps/account/serializers/online_book.py
@@ -7,21 +7,49 @@ from ..models.online_book import OnlineBook
 User = get_user_model()
 
 
+# class OnlineBookSerializer(serializers.ModelSerializer):
+#     rating = serializers.IntegerField()
+
+#     class Meta:
+#         model = OnlineBook
+#         fields = (
+#             "guid",
+#             "book",
+#             "book_type",
+#             "rating",
+#         )
+
+#     def to_representation(self, instance):
+#         self.fields["book"] = BookHelperSerializer()
+#         return super().to_representation(instance)
+
+
 class OnlineBookSerializer(serializers.ModelSerializer):
-    rating = serializers.IntegerField()
+    book_guid = serializers.ReadOnlyField(source='book.guid')
+    book = serializers.ReadOnlyField(source='book.title')
+    author = serializers.ReadOnlyField(source='book.author')
+    publisher = serializers.ReadOnlyField(source='book.publisher')
+    published_date = serializers.ReadOnlyField(source='book.published_date')
+    thumbnail = serializers.ImageField(source='book.thumbnail', read_only=True)
+    category= serializers.ReadOnlyField(source='book.category.title')
+    get_review = serializers.ReadOnlyField(source='book.get_review')
+    # rating = serializers.IntegerField()
 
     class Meta:
         model = OnlineBook
         fields = (
             "guid",
+            'book_guid',
+            "category",
             "book",
+            'author',
+            'publisher',
+            'published_date',
             "book_type",
-            "rating",
+            'thumbnail',
+            'get_review',
+            'state'
         )
-
-    def to_representation(self, instance):
-        self.fields["book"] = BookHelperSerializer()
-        return super().to_representation(instance)
 
 
 class ContentListSerializer(serializers.Serializer):
