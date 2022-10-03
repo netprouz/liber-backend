1b73fc0d68f8a0529c80d51390bc65ee79f47551
akhad97 <ahadjon.abdullaev1997@gmail.com>
2022-10-03 14:47:50 +0500

    changes

--- main/apps/book/serializers/book.py
+++ main/apps/book/serializers/book.py
@@ -149,7 +149,7 @@ class BookDetailSerializer(serializers.ModelSerializer):
             'short_description_ru',
             "published_date",
             "types",
-            'purchased',
+            # 'purchased',
             "created_at"
         )
 
@@ -163,8 +163,8 @@ class BookDetailSerializer(serializers.ModelSerializer):
         self.fields["category"] = CategoryHelperSerializer()
         return super().to_representation(instance)
 
-    def get_purchased(self, obj):
-        return obj.set_purchased_book_types_true(self.user)
+    # def get_purchased(self, obj):
+    #     return obj.set_purchased_book_types_true(self.user)
 
 
 class BookUpdateSerializer(serializers.ModelSerializer):
